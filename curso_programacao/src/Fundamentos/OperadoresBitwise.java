d27cb2b9e7fbc605c3312725c3e1e31170993c1a
dudinhaa-luz <eduardaluz006@gmail.com>
2021-08-30 08:36:58 -0300

    Fundamentos

---
+++ curso_programacao/src/Fundamentos/OperadoresBitwise.java
@@ -0,0 +1,34 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class OperadoresBitwise {
+
+	public static void main(String[] args) {
+		//& - operador E em bitwise
+		//| - operador OU em bitwise
+		//^ - operador OU EXCLUSIVO em bitwise
+		//Será realizada a comparação bit a bit
+		
+		int n1 = 89;
+		int n2 = 60;
+		
+		System.out.println(n1 & n2);
+		System.out.println(n1 | n2);
+		System.out.println(n1 ^ n2);
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int mask = 0b100000; //32
+		int n = sc.nextInt();
+		
+		if ((n & mask) != 0) {
+			System.out.println("6th bit is true!");//113
+		}else {
+			System.out.println("6th bit is false!");//89
+		}
+		
+		sc.close();
+	}
+
+}
